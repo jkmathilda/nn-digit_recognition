7a65869f8b50b13b5a76fe2e4a8ffc832ae20683
jkmathilda <jkmathilda627@gmail.com>
2024-07-17 22:01:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+# Start Neural Network
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))
